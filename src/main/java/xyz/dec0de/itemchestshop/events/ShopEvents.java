28007778d66bd33bfab47abd0a35d9d048237cf1
Shaun Agostinho <shaunagostinho@gmail.com>
2020-04-09 13:17:46 -0700

    fixed negative numbers

--- src/main/java/xyz/dec0de/itemchestshop/events/ShopEvents.java
+++ src/main/java/xyz/dec0de/itemchestshop/events/ShopEvents.java
@@ -1,6 +1,5 @@
 package xyz.dec0de.itemchestshop.events;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -44,120 +43,121 @@ public class ShopEvents implements Listener {
                             int costAmount = Integer.parseInt(costs[1]);
                             Material costItem = validMaterial(costItemName);
 
-                            // Get chest attached to sign
-                            for (BlockFace f : (new BlockFace[]{BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST})) {
-                                Block relativeBlock = block.getRelative(f);
-                                if (relativeBlock.getType() == Material.CHEST) {
-                                    Chest chest = (Chest) relativeBlock.getState();
+                            if (costAmount >= 1 && forSaleAmount >= 1)
+                                // Get chest attached to sign
+                                for (BlockFace f : (new BlockFace[]{BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST})) {
+                                    Block relativeBlock = block.getRelative(f);
+                                    if (relativeBlock.getType() == Material.CHEST) {
+                                        Chest chest = (Chest) relativeBlock.getState();
 
-                                    Inventory chestInventory = chest.getInventory();
+                                        Inventory chestInventory = chest.getInventory();
 
-                                    Player player = event.getPlayer();
-                                    Inventory playerInventory = player.getInventory();
+                                        Player player = event.getPlayer();
+                                        Inventory playerInventory = player.getInventory();
 
-                                    // Check if player has enough for the cost
-                                    if (playerInventory.containsAtLeast(new ItemStack(costItem), costAmount)) {
+                                        // Check if player has enough for the cost
+                                        if (playerInventory.containsAtLeast(new ItemStack(costItem), costAmount)) {
 
-                                        // Check if the chest has enough to sell
-                                        if (chestInventory.containsAtLeast(new ItemStack(forSaleItem), forSaleAmount)) {
+                                            // Check if the chest has enough to sell
+                                            if (chestInventory.containsAtLeast(new ItemStack(forSaleItem), forSaleAmount)) {
 
-                                            // A horrible thing
-                                            List<ItemStack> chestList = new ArrayList<>();
-                                            for(ItemStack itemStackFor : chestInventory.getContents()){
-                                                if(itemStackFor != null)
-                                                    chestList.add(itemStackFor);
-                                            }
-                                            ItemStack[] chestContents = chestList.toArray(new ItemStack[chestList.size()]);
-
-                                            // Check chest for items to be removed
-                                            for (ItemStack i : chestContents) {
-                                                if (i.getType() == forSaleItem && i.getAmount() >= forSaleAmount) {
-                                                    // Check if chest has room for shop
-                                                    if (chestInventory.firstEmpty() != -1) {
-
-                                                        // A horrible thing
-                                                        List<ItemStack> playerList = new ArrayList<>();
-                                                        for(ItemStack itemStackFor : playerInventory.getContents()){
-                                                            if(itemStackFor != null)
-                                                                playerList.add(itemStackFor);
-                                                        }
-                                                        ItemStack[] playerContents = playerList.toArray(new ItemStack[playerList.size()]);
-
-                                                        // Check player inventory for items to be removed
-                                                        for (ItemStack is : playerContents) {
-                                                            if (is.getType() == costItem && is.getAmount() >= costAmount) {
-
-                                                                // Give player item
-                                                                if (playerInventory.firstEmpty() != -1) {
-                                                                    // Remove forSale from Chest
-                                                                    if (i.getAmount() == forSaleAmount) {
-                                                                        chestInventory.setItem(chestInventory.first(i), new ItemStack(Material.AIR));
-                                                                    } else {
-                                                                        i.setAmount(i.getAmount() - forSaleAmount);
-                                                                    }
-
-                                                                    // Remove cost from player inventory
-                                                                    if (is.getAmount() == costAmount) {
-                                                                        playerInventory.setItem(playerInventory.first(i), new ItemStack(Material.AIR));
-                                                                    } else {
-                                                                        is.setAmount(is.getAmount() - costAmount);
-                                                                    }
-
-                                                                    // Add cost to chest inventory
-                                                                    chestInventory.addItem(new ItemStack(costItem, costAmount));
-                                                                    //chest.update();
-
-                                                                    // Add forSale to player inventory
-                                                                    playerInventory.addItem(new ItemStack(forSaleItem, forSaleAmount));
-                                                                    player.updateInventory();
-
-                                                                    player.sendMessage(
-                                                                            ChatColor.translateAlternateColorCodes('&',
-                                                                                    "&aThank you for your business. Come again."));
-                                                                } else {
-                                                                    //player.getWorld().dropItemNaturally(player.getLocation(), forSaleItem);
-
-                                                                    player.sendMessage(
-                                                                            ChatColor.translateAlternateColorCodes('&',
-                                                                                    "&cYou do not have any room in your inventory."));
-                                                                }
-                                                                break;
-                                                            }
-                                                        }
-                                                    } else {
-                                                        // Chest does not have enough room
-                                                        //player.getWorld().dropItemNaturally(player.getLocation(), forSaleItem);
-                                                        player.sendMessage(
-                                                                ChatColor.translateAlternateColorCodes('&',
-                                                                        "&cThis shop does not have enough room to accept your purchase."));
-                                                    }
-
-
-                                                    break;
+                                                // A horrible thing
+                                                List<ItemStack> chestList = new ArrayList<>();
+                                                for(ItemStack itemStackFor : chestInventory.getContents()){
+                                                    if(itemStackFor != null)
+                                                        chestList.add(itemStackFor);
                                                 }
+                                                ItemStack[] chestContents = chestList.toArray(new ItemStack[chestList.size()]);
+
+                                                // Check chest for items to be removed
+                                                for (ItemStack i : chestContents) {
+                                                    if (i.getType() == forSaleItem && i.getAmount() >= forSaleAmount) {
+                                                        // Check if chest has room for shop
+                                                        if (chestInventory.firstEmpty() != -1) {
+
+                                                            // A horrible thing
+                                                            List<ItemStack> playerList = new ArrayList<>();
+                                                            for(ItemStack itemStackFor : playerInventory.getContents()){
+                                                                if(itemStackFor != null)
+                                                                    playerList.add(itemStackFor);
+                                                            }
+                                                            ItemStack[] playerContents = playerList.toArray(new ItemStack[playerList.size()]);
+
+                                                            // Check player inventory for items to be removed
+                                                            for (ItemStack is : playerContents) {
+                                                                if (is.getType() == costItem && is.getAmount() >= costAmount) {
+
+                                                                    // Give player item
+                                                                    if (playerInventory.firstEmpty() != -1) {
+                                                                        // Remove forSale from Chest
+                                                                        if (i.getAmount() == forSaleAmount) {
+                                                                            chestInventory.setItem(chestInventory.first(i), new ItemStack(Material.AIR));
+                                                                        } else {
+                                                                            i.setAmount(i.getAmount() - forSaleAmount);
+                                                                        }
+
+                                                                        // Remove cost from player inventory
+                                                                        if (is.getAmount() == costAmount) {
+                                                                            playerInventory.setItem(playerInventory.first(i), new ItemStack(Material.AIR));
+                                                                        } else {
+                                                                            is.setAmount(is.getAmount() - costAmount);
+                                                                        }
+
+                                                                        // Add cost to chest inventory
+                                                                        chestInventory.addItem(new ItemStack(costItem, costAmount));
+                                                                        //chest.update();
+
+                                                                        // Add forSale to player inventory
+                                                                        playerInventory.addItem(new ItemStack(forSaleItem, forSaleAmount));
+                                                                        player.updateInventory();
+
+                                                                        player.sendMessage(
+                                                                                ChatColor.translateAlternateColorCodes('&',
+                                                                                        "&aThank you for your business. Come again."));
+                                                                    } else {
+                                                                        //player.getWorld().dropItemNaturally(player.getLocation(), forSaleItem);
+
+                                                                        player.sendMessage(
+                                                                                ChatColor.translateAlternateColorCodes('&',
+                                                                                        "&cYou do not have any room in your inventory."));
+                                                                    }
+                                                                    break;
+                                                                }
+                                                            }
+                                                        } else {
+                                                            // Chest does not have enough room
+                                                            //player.getWorld().dropItemNaturally(player.getLocation(), forSaleItem);
+                                                            player.sendMessage(
+                                                                    ChatColor.translateAlternateColorCodes('&',
+                                                                            "&cThis shop does not have enough room to accept your purchase."));
+                                                        }
+
+
+                                                        break;
+                                                    }
+                                                }
+                                            } else {
+                                                // Player does not have enough for the cost
+                                                player.sendMessage(
+                                                        ChatColor.translateAlternateColorCodes('&',
+                                                                "&cThis shop does not have enough items to sell you."));
                                             }
                                         } else {
                                             // Player does not have enough for the cost
                                             player.sendMessage(
                                                     ChatColor.translateAlternateColorCodes('&',
-                                                            "&cThis shop does not have enough items to sell you."));
+                                                            "&cYou do not have enough items to purchase this."));
                                         }
-                                    } else {
-                                        // Player does not have enough for the cost
-                                        player.sendMessage(
-                                                ChatColor.translateAlternateColorCodes('&',
-                                                        "&cYou do not have enough items to purchase this."));
+                                        break;
                                     }
-                                    break;
                                 }
-                            }
+
+                            event.setCancelled(true);
 
                             // Handle amount parse error
                         } catch (Exception exception) {
                         }
                     }
-
-                    event.setCancelled(true);
                 }
             }
         }
